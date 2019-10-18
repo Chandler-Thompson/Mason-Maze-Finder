af32810c6cd7943b1a2ba027363978e995f2c03a
poobear0803 <55003023+poobear0803@users.noreply.github.com>
2019-10-18 17:01:38 -0400

    Add files via upload

---
+++ CS321CourseProject/src/UI/Node.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Node {
+    private List <Node> nodeList = new ArrayList<Node>();
+    private int id;
+    private String nodeLabel;
+
+    public Node(int id, String label, List<Node> nodeList)
+    {
+        this.id = id;
+        this.nodeList = nodeList;
+        nodeLabel = label;
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public List<Node> getNodeList()
+    {
+        return nodeList;
+    }
+
+    public String getNodeLabel()
+    {
+        return nodeLabel;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNodeLabel(String label)
+    {
+        nodeLabel = label;
+    }
+
+}
