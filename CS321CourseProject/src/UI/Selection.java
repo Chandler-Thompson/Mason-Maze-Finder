8f610b36a27000c460648a55a2626b3372710e00
Chandler-Thompson <iamdrachir@gmail.com>
2019-10-22 19:54:12 -0400

    Created:
    ClickableNode: To be used in creation of interactable map to more easily
    access nodes at given points on map.
    
    Selection: Holds an ArrayList of multiple ClickableNodes to be used in
    implementation of interactable map.
    
    Modified:
    
    Node: added enum Terrain (currently only WALKABLE and BLOCKED values)
    also changed some method names for easier typing (i.e. getNodeLabel() ->
    getLabel())
    
    MapPanel: added @suppresswarnings("serial")
    
    MainFrame: added @suppresswarnings("serial")
    
    MainWindow: added @suppresswarnings("serial")

---
+++ CS321CourseProject/src/UI/Selection.java
@@ -0,0 +1,26 @@
+package UI;
+
+import java.util.ArrayList;
+
+/***
+ * 
+ * Class for holding and manipulating multiple ClickableNodes at once.
+ * 
+ * Will be used for implementing user-defined NoGo Areas
+ * 
+ * @author arari
+ *
+ */
+public class Selection {
+
+	ArrayList<ClickableNode> nodes;
+	
+	public Selection(ArrayList<ClickableNode> nodes) {
+		this.nodes = nodes;
+	}
+	
+	public ArrayList<ClickableNode> getNodes(){
+		return nodes;
+	}
+	
+}
